2b52c5b086c6decad1e0353398aa69b47c026394
Adib Khaled <adibkhaled@gmail.com>
2019-11-28 23:01:30 +0100

    Learn about if

---
+++ MainClass.rb
@@ -0,0 +1,9 @@
+require 'PrintClass'
+
+class MainClass
+
+  test = PrintClass.sendLocation
+  test2 = PrintClass.new('test')
+  puts test
+  puts test2.login 
+end
--- PrintClass.rb
+++ PrintClass.rb
@@ -1,26 +1,38 @@
 require 'ClassA'
 require 'java'
-java_import java.lang.System 
+
+java_import java.lang.System
 java_import javax.swing.JFrame
 java_import javax.swing.JLabel
 
 class PrintClass
-  @@x
-  @@y
-  location00 = ClassA.getName01()
-  location10 = ClassA.getName02()
+  
+  attr_accessor :name
+  
+  def initialize(name)
+      @name = name
+    end
+
+  @@location00 = ClassA.getName01()
+  @@location10 = ClassA.getName02()
+  @@x = ''
   #puts testrun.to_s.split(",")
   #testrun.each { |size, loc |  puts "#{loc}" }
-  @@x= location10.values[0]
-  @@y= location10.values[1]
-  puts @@x.to_i
-  puts @@y.to_i
-  frame = JFrame.new("Window")
-  label = JLabel.new("Hello")
-
-  # We can transparently call Java methods on Java objects, just as if they were defined in Ruby.
-  frame.add(label)  # Invoking the Java method 'add'.
-  frame.setDefaultCloseOperation(JFrame::EXIT_ON_CLOSE)
-  frame.pack
-  frame.setVisible(true)
+  #@@x= location10.values[0]
+  #@@y= location10.values[1]
+  
+  def self.sendLocation
+    @@x = @@location10.values[0]
+    return @@x
+  end
+  
+  def login
+    if @name == 'Adib'
+      puts @name
+    else
+      puts "hahaha"
+    end
+  end
+  
+    
 end
